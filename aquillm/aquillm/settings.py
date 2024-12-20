ace135a519d2b2441c59d86500b1172f0ed43e39
Chandler Campbell <r.chandler.campbell@gmail.com>
2024-12-20 19:12:16 +0000

    add beta to allowed hosts

--- aquillm/aquillm/settings.py
+++ aquillm/aquillm/settings.py
@@ -193,7 +193,7 @@ INTERNAL_IPS = [
 X_FRAME_OPTIONS = "SAMEORIGIN"
 USE_TZ=True
 DATA_UPLOAD_MAX_MEMORY_SIZE=  268435456
-ALLOWED_HOSTS =['aquillm.space', 'www.aquillm.space', 'localhost', 'alpha.aquillm.space']
+ALLOWED_HOSTS =['aquillm.space', 'www.aquillm.space', 'localhost', 'alpha.aquillm.space', 'beta.aquillm.space']
 
 ASGI_APPLICATION = "aquillm.asgi.application"
 from google.oauth2 import service_account
